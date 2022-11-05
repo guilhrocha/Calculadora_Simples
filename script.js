80feca4a4336055225a55dd1b05360782b6839d9
Guilherme Rocha <105439209+guilhrocha@users.noreply.github.com>
2022-11-05 12:33:09 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,25 @@
+//Faz a soma
+
 function insert(num)
         {
             var numero = document.getElementById('resultado').innerHTML;
             document.getElementById('resultado').innerHTML = numero + num;
         }
+
+//Limpa a caixa de resultado
         function clean()
         {
             document.getElementById('resultado').innerHTML = "";
         }
+
+//Exclui uma casa
         function back()
         {
             var resultado = document.getElementById('resultado').innerHTML;
             document.getElementById('resultado').innerHTML = resultado.substring(0, resultado.length -1);
         }
+
+//Faz o calculo e joga no campo "resultado"
         function calcular()
         {
             var resultado = document.getElementById('resultado').innerHTML;
